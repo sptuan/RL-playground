1180b6ec41a83707193b396a5c68b5dabe4063a5
SPtuan <sptuan@steinslab.io>
2018-11-06 21:05:38 +0800

    Complete DQN_Maze

--- 02_DQN/maze/run_this.py
+++ 02_DQN/maze/run_this.py
@@ -5,21 +5,23 @@ from RL_brain import DeepQNetwork
 def run_maze():
     step = 0
     for episode in range(300):
-        # initial observation
+        # initial observation 初始化环境
         observation = env.reset()
 
         while True:
-            # fresh env
+            # fresh env 刷新环境
             env.render()
 
-            # RL choose action based on observation
+            # RL choose action based on observation 根据观察选择行动
             action = RL.choose_action(observation)
 
-            # RL take action and get next observation and reward
+            # RL take action and get next observation and reward 给环境，返回奖励
             observation_, reward, done = env.step(action)
 
+            # DQN存储记忆
             RL.store_transition(observation, action, reward, observation_)
 
+            # 控制学习的起始时间和频率（先积累一些记忆再学习）
             if (step > 200) and (step % 5 == 0):
                 RL.learn()
 
@@ -43,10 +45,10 @@ if __name__ == "__main__":
                       learning_rate=0.01,
                       reward_decay=0.9,
                       e_greedy=0.9,
-                      replace_target_iter=200,
-                      memory_size=2000,
-                      # output_graph=True
+                      replace_target_iter=200, # 每200步替换一次 target_net的参数
+                      memory_size=2000, # 记忆上限
+                      # output_graph=True # 是否输出tensorboard文件
                       )
     env.after(100, run_maze)
     env.mainloop()
-    RL.plot_cost()
+    RL.plot_cost() # 观看神经网络的误差曲线
