6a7556bdcdccd34ec977c5f2c4aceba2f06151fb
SPtuan <sptuan@steinslab.io>
2018-11-06 14:25:05 +0800

    Create TF104

--- 02_DQN/TensorFlow101/104_layer.py
+++ 02_DQN/TensorFlow101/104_layer.py
@@ -5,27 +5,44 @@
 import tensorflow as tf
 import numpy as np
 
-def add_layer(inputs, in_size,out_size,activation_function = None)
-    Weight = tf.Variable(tf.random_normal([[in_size,out_size]]))
-    biases = tf.Variable(tf.zeros([1,out_size])+0.1)
-    Wx_plus_b = tf.natmul(inputs, Weight) + biases
-
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_function is None:
         outputs = Wx_plus_b
-    else
+    else:
         outputs = activation_function(Wx_plus_b)
-
     return outputs
 
 
-x_data = np.linspace(-1,1,300, dtype=np.float32)[:, np.newaxis]
+x_data = np.linspace(-1, 1, 300, dtype=np.float32)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
 y_data = np.square(x_data) - 0.5 + noise
 
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
 
+# 建层
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
-perdition = add_layer(l1, 10, 1, activation_function = None)
+prediction = add_layer(l1, 10, 1, activation_function=None)
 
+# 误差计算，两者平方取平均
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),reduction_indices=[1]))
 
+# 训练参数
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+# 开始初始化
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+# 开始训练
+for i in range(10000):
+    # training
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        # 输出误差
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
